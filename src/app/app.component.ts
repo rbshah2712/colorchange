23b292258652286b85ce026d7ba4ee6e52e9e635
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-03-15 10:45:29 -0400

    hovercolor style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'colorchange';
+  theme :string = '';
+  document: any;
+
+  firstname = "ruchi";
+  check = true;
+  hoverColorVar: string = '';
+
+  changecolor(color:string){
+    this.theme = color; 
+    
+  }
 }
